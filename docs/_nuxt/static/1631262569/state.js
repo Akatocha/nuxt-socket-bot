e6d52177f336ff4ebfce11f2de9fc53791953872
Гудима Роман <hudymaroma.intertech@gmail.com>
2021-09-10 11:29:43 +0300

    fix: :bug: url

---
+++ docs/_nuxt/static/1631262569/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-socket-bot\u002F_nuxt\u002Fstatic\u002F1631262569",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fnuxt-socket-bot\u002F",assetsPath:"\u002Fnuxt-socket-bot\u002F_nuxt\u002F",cdnURL:a}}}}(null));
